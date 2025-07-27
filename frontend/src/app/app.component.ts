a8f2c96628bfc69c81d7f3e8d45c29cfdd161a24
KUNSALISA <attawerapathsalisa@gmail.com>
2025-07-27 17:10:18 +0700

    logout

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router } from '@angular/router';
+import { LoginResponse } from './login';
+import { CommonModule } from '@angular/common';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  
+  user: LoginResponse | null = null;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const storedUser = localStorage.getItem('user');
+    if (storedUser) {
+      this.user = JSON.parse(storedUser) as LoginResponse;
+    }
+  }
+
+  logout(): void {
+    localStorage.removeItem('user');
+    this.user = null;
+    this.router.navigate(['/login']);
+  }
 }
