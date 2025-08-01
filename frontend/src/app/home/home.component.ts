1ba5b361c9c5960edf0b1386eff4646115eb9c8d
KUNSALISA <attawerapathsalisa@gmail.com>
2025-08-01 19:53:13 +0700

    front

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DatePipe } from '@angular/common';
 import { RoomMapComponent } from '../room-map/room-map.component';
+import { SchedulerCardComponent } from '../scheduler-card/scheduler-card.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, DatePipe, RoomMapComponent],
+  imports: [RouterModule, DatePipe, RoomMapComponent, SchedulerCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
